40c113814b8df5f7990cc21d89e1554c052c2895
LucksonBuleya <139395902+LucksonBuleya@users.noreply.github.com>
2026-02-19 19:41:30 +0200

    feat(auth): add bcrypt password hashing for registration
    - Integrated bcrypt to hash user passwords before saving
    - Set salt rounds to 10 for secure and efficient hashing
    - Updated registration route to store hashed passwords
    - Ensures plaintext passwords are never saved in DB

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import User from "../model/User.js";
+import bcrypt from 'bcryptjs';
 import { registerValidation } from '../validation.js';
+
 
 //creating a router instance to define routes
 const router = express.Router(); 
@@ -12,21 +14,29 @@ router.post('/register', async (req, res) => {
     const { error } = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     
-    //Creating a new user instance with the request body data
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password 
-
-    });
 
     //checking if the email already exists in the database
     const emailExists = await User.findOne({ email: req.body.email });
     if (emailExists) return res.status(400).send('Email already exists');
     
+    //Hashing the password before saving the user to the database
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Creating a new user instance with the request body data
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashPassword 
+    });
+
+
     //saving the user to the database
     try {
         const savedUser = await user.save();
+        //in a real application, you wouldn't want to send the entire user object back to the client, 
+        // especially not the password. 
+        // You might want to send an id or a token instead. But for demonstration purposes, we'll send the saved user object back.
         res.send(savedUser);
     }catch(err){
     if (err.code === 11000) { //incase of duplicate key error
